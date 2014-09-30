25e4ca4743ca671cb5bfbdf135860bb1149914bd
pipeclap <pipeclap@gmail.com>
2014-09-30 10:46:52 -0400

    some add coding but is not working good

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
+var salesDetails = $('.sales__details');
+var Sales = $('.sales');
+
+var salesClickHandler = function (){
+	var isActive = salesDetails.attr('data-state');
+
+	if (isActive == 'active') {
+	    salesDetails.attr('data-state', 'inactive');
+	} else{
+		salesDetails.attr('data-state', 'active');
+	}	
+};
 
 
-
-
-
-
+Sales.on('click', salesClickHandler);
 
 
