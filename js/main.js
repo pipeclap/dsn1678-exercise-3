374ceedf309e9f4229b204884c4131f6d206634a
pipeclap <pipeclap@gmail.com>
2014-09-30 11:32:12 -0400

    change everything for next button

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ var salesLeftClickHandler = function (){
 
 salesLeft.on('click', salesLeftClickHandler);
 
+
+
+var salesDetailright = $('.salesdetailsright');
+var salesRight = $('.salesright');
+
+var salesRightClickHandler = function (){
+	var isActive = salesdetailsright.attr('data-state');
+
+	if (isActive == 'active') {
+	    salesDetailsright.attr('data-state', 'inactive');
+	} else{
+		salesDetailsright.attr('data-state', 'active');
+	}	
+};
+
+
+salesRight.on('click', salesRightClickHandler);
